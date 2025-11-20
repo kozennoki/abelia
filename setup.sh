c45311e384944e57a42a36c23b6810f885727225
kozennoki <kozennoki006@gmail.com>
2025-11-20 13:31:04 +0000

    :+1: miseで管理するよう更新

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+# Install mise
+curl -sSfL https://mise.run | sh
+
+# Add mise activation to bashrc
+echo 'eval "$(~/.local/bin/mise activate bash)"' >> ~/.bashrc
+
+# Install mise tools
+~/.local/bin/mise trust
+~/.local/bin/mise install
